2cde1e1f09aaab6cc7b1daa37d65d88d45ceb01c
satvk <satvikkatoch@gmail.com>
2024-10-20 22:25:57 +0530

    added back react mode

--- task-reward-app/src/index.js
+++ task-reward-app/src/index.js
@@ -13,7 +13,11 @@ import "admin-lte/dist/js/adminlte.min.js";
 import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Use the bundled version that includes Popper.js
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
